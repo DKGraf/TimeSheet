6d8dec8ff261e68d8b0bd8025155688c37d9b5f2
DKGraf <semyon.blokhin@gmail.com>
2018-03-12 21:22:15 +0700

    Why SOF???

--- src/main/java/org/astanis/Main.java
+++ src/main/java/org/astanis/Main.java
@@ -4,69 +4,83 @@ import org.astanis.db.datasets.EmployeeDataSet;
 import org.astanis.db.datasets.WorkingTimeDataSet;
 import org.astanis.db.dbservice.DbServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> m201801 = List.of(0, 0, 0, 0, 0, 0, 0, 0, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480);
-        List<Integer> m201802 = List.of(480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 420, 0, 0, 0, 480, 480, 480);
-        List<Integer> m201803 = List.of(480, 480, 0, 0, 480, 480, 420, 0, 0, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0);
+//        List<Integer> m201801 = List.of(0, 0, 0, 0, 0, 0, 0, 0, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480);
+//        List<Integer> m201802 = List.of(480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 420, 0, 0, 0, 480, 480, 480);
+//        List<Integer> m201803 = List.of(480, 480, 0, 0, 480, 480, 420, 0, 0, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0, 0, 480, 480, 480, 480, 480, 0);
+//
+//        List<WorkingTimeDataSet> list01 = new ArrayList<>();
+//        for (int i = 0; i < m201801.size(); i++) {
+//            list01.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
+//        }
+//        for (int i = 0; i < m201802.size(); i++) {
+//            list01.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
+//        }
+//        for (int i = 0; i < m201803.size(); i++) {
+//            list01.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
+//        }
+//
+//        List<WorkingTimeDataSet> list02 = new ArrayList<>();
+//        for (int i = 0; i < m201801.size(); i++) {
+//            list02.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
+//        }
+//        for (int i = 0; i < m201802.size(); i++) {
+//            list02.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
+//        }
+//        for (int i = 0; i < m201803.size(); i++) {
+//            list02.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
+//        }
+//
+//        List<WorkingTimeDataSet> list03 = new ArrayList<>();
+//        for (int i = 0; i < m201801.size(); i++) {
+//            list03.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
+//        }
+//        for (int i = 0; i < m201802.size(); i++) {
+//            list03.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
+//        }
+//        for (int i = 0; i < m201803.size(); i++) {
+//            list03.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
+//        }
+//
+//        EmployeeDataSet employee1 = new EmployeeDataSet
+//                ("Ivanov Ivan Ivanovich", 1L, "Some Department", list01, true);
+//        EmployeeDataSet employee2 = new EmployeeDataSet
+//                ("Petrov Petr Petrovich", 2L, "Another Department", list02, true);
+//        EmployeeDataSet employee3 = new EmployeeDataSet
+//                ("Ivanova Olga Ivanovna", 3L, "Another Department", list03, false);
+//
+//        for (WorkingTimeDataSet aList01 : list01) {
+//            aList01.setEmployee(employee1);
+//        }
+//        for (WorkingTimeDataSet aList02 : list02) {
+//            aList02.setEmployee(employee2);
+//        }
+//        for (WorkingTimeDataSet aList03 : list03) {
+//            aList03.setEmployee(employee3);
+//        }
+        WorkingTimeDataSet wdt = new WorkingTimeDataSet(2018, 1, 1, 640);
 
-        List<WorkingTimeDataSet> list01 = new ArrayList<>();
-        for (int i = 0; i < m201801.size(); i++) {
-            list01.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
-        }
-        for (int i = 0; i < m201802.size(); i++) {
-            list01.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
-        }
-        for (int i = 0; i < m201803.size(); i++) {
-            list01.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
-        }
+        EmployeeDataSet employee1 = new EmployeeDataSet();
 
-        List<WorkingTimeDataSet> list02 = new ArrayList<>();
-        for (int i = 0; i < m201801.size(); i++) {
-            list02.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
-        }
-        for (int i = 0; i < m201802.size(); i++) {
-            list02.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
-        }
-        for (int i = 0; i < m201803.size(); i++) {
-            list02.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
-        }
+//        wdt.setEmployee(employee1);
 
-        List<WorkingTimeDataSet> list03 = new ArrayList<>();
-        for (int i = 0; i < m201801.size(); i++) {
-            list03.add(new WorkingTimeDataSet(2018, 1, i, m201801.get(i)));
-        }
-        for (int i = 0; i < m201802.size(); i++) {
-            list03.add(new WorkingTimeDataSet(2018, 2, i, m201802.get(i)));
-        }
-        for (int i = 0; i < m201803.size(); i++) {
-            list03.add(new WorkingTimeDataSet(2018, 3, i, m201803.get(i)));
-        }
+        employee1.setWorkingHours(List.of(wdt));
+        employee1.setName("AAAAA");
+        employee1.setDepartment("BBBBB");
+        employee1.setGender(true);
+        employee1.setPersonalNumber(1);
 
-        EmployeeDataSet employee1 = new EmployeeDataSet
-                ("Ivanov Ivan Ivanovich", 1L, "Some Department", list01, true);
-        EmployeeDataSet employee2 = new EmployeeDataSet
-                ("Petrov Petr Petrovich", 2L, "Another Department", list02, true);
-        EmployeeDataSet employee3 = new EmployeeDataSet
-                ("Ivanova Olga Ivanovna", 3L, "Another Department", list03, false);
-
-        for (WorkingTimeDataSet aList01 : list01) {
-            aList01.setEmployee(employee1);
-        }
-        for (WorkingTimeDataSet aList02 : list02) {
-            aList02.setEmployee(employee2);
-        }
-        for (WorkingTimeDataSet aList03 : list03) {
-            aList03.setEmployee(employee3);
-        }
 
         DbServiceImpl dbService = new DbServiceImpl();
 
         dbService.save(employee1);
-        dbService.save(employee2);
-        dbService.save(employee3);
+//        dbService.save(employee2);
+//        dbService.save(employee3);
+
+        System.out.println(dbService.readById(1));
+//        System.out.println(dbService.readByPersonalNumber(2));
     }
 }
--- src/main/java/org/astanis/db/datasets/dao/EmployeeDataSetDAO.java
+++ src/main/java/org/astanis/db/datasets/dao/EmployeeDataSetDAO.java
@@ -2,9 +2,11 @@ package org.astanis.db.datasets.dao;
 
 import org.astanis.db.datasets.EmployeeDataSet;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class EmployeeDataSetDAO {
@@ -23,28 +25,35 @@ public class EmployeeDataSetDAO {
     }
 
     public EmployeeDataSet readByPersonalNumber(long personalNumber) {
-        final CriteriaBuilder builder = session.getCriteriaBuilder();
-        final CriteriaQuery<EmployeeDataSet> criteria = builder.createQuery(EmployeeDataSet.class);
-
+//        final CriteriaBuilder builder = session.getCriteriaBuilder();
+//        final CriteriaQuery<EmployeeDataSet> criteria = builder.createQuery(EmployeeDataSet.class);
+//        final Root<EmployeeDataSet> root = criteria.from(EmployeeDataSet.class);
+//        criteria.
+//        criteria.where(builder.equal(root.get("personalNumber"), personalNumber));
+//        Query<EmployeeDataSet> query = session.createQuery(criteria);
+        Query query = session.createQuery("FROM EmployeeDataSet WHERE personalNumber = :PN");
+        query.setParameter("PN", personalNumber);
+        EmployeeDataSet employee = (EmployeeDataSet) query.uniqueResult();
+        return employee;
     }
 
     public EmployeeDataSet readByName(String name) {
-
+        return null;
     }
 
     public EmployeeDataSet readByDepartment(String department) {
-
+        return null;
     }
 
     public List<EmployeeDataSet> readAll() {
-
+        return null;
     }
 
     public List<EmployeeDataSet> readAllByDepartment(String department) {
-
+        return null;
     }
 
     public List<EmployeeDataSet> readAllByGender(boolean gender) {
-
+        return null;
     }
 }
